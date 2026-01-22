78200fe58534251b5a8b7025f47bd0988f279106
tegarbimop <beigardino@gmail.com>
2026-01-22 10:34:10 +0700

    session-3

--- app/(landing)/components/ui/card-with-header.tsx
+++ app/(landing)/components/ui/card-with-header.tsx
@@ -5,7 +5,7 @@ type TCardWithHeaderProps = {
 
 const CardWithHeader = ({ title, children }: TCardWithHeaderProps) => {
   return (
-    <div className="bg-white">
+    <div className="bg-white ">
       <div className="px-5 py-4 border-b border-gray-200">
         <h2 className="font-bold text-lg">{title}</h2>
       </div>
